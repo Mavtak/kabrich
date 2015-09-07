e69716776a0eb0b32bca168b37e67df26d0138c4
David McGrath <github.com@davidmcgrath.com>
2015-09-07 16:12:22 -0400

    add kabrich.slice module

--- src/angular/modules.js
+++ src/angular/modules.js
@@ -1,7 +1,11 @@
 angular.module('kabrich', [
   'kabrich.layout',
+  'kabrich.slice',
   'kabrich.templates',
 ]);
 
 angular.module('kabrich.layout', [
+]);
+
+angular.module('kabrich.slice', [
 ]);
